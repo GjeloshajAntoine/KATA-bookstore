8a696eb0f6ad435d9c41997fbba827ff2346935f
Antoine <antoine.gjeloshaj@fedasil.be>
2024-12-10 17:58:24 +0100

    Add cors definition

--- Backend/src/main/java/com/example/BookStore/Controllers/BookController.java
+++ Backend/src/main/java/com/example/BookStore/Controllers/BookController.java
@@ -3,6 +3,7 @@ package com.example.BookStore.Controllers;
 import com.example.BookStore.Models.Book;
 import com.example.BookStore.Repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ public class BookController {
     BookRepository bookRepository;
 
     @GetMapping("/api/books")
+    @CrossOrigin(origins = "http://localhost:5173/")
     public List<Book> books() {
         return bookRepository.findAll();
     }
