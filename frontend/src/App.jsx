ee43e278c6acc45456f40f868a8d0db128866925
Antoine <antoine.gjeloshaj@fedasil.be>
2024-12-10 18:44:54 +0100

    Add cart page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import { Route, Routes } from 'react-router'
+import { Route, Routes, } from 'react-router'
 import './App.css'
 import { Home } from './pages/Home'
+import { Cart } from './pages/Cart'
 
 function App() {
   return (
         <Routes>
             <Route path='/' Component={Home}/>
+            <Route path='/cart' Component={Cart}/>
         </Routes>
   )
 }
