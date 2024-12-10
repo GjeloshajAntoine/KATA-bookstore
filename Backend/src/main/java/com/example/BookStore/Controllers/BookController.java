80044c66545d45194452f2516a59968bb984da0c
Antoine <antoine.gjeloshaj@fedasil.be>
2024-12-10 14:30:32 +0100

    Fix controller

--- Backend/src/main/java/com/example/BookStore/Controllers/BookController.java
+++ Backend/src/main/java/com/example/BookStore/Controllers/BookController.java
@@ -4,9 +4,11 @@ import com.example.BookStore.Models.Book;
 import com.example.BookStore.Repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class BookController {
 
     @Autowired
