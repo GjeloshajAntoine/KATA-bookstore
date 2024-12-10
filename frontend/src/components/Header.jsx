71351cf63a97c8032b8bbfc2020afad8119fe265
Antoine <antoine.gjeloshaj@fedasil.be>
2024-12-10 16:26:35 +0100

    Add test for header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export function Header() {
+    return( <div>
+        <h1>Book Store</h1>
+    </div>);
+}
---
+++ frontend/src/tests/components/header.test.jsx
@@ -0,0 +1,9 @@
+import { Header } from "../../components/Header";
+import { render } from '@testing-library/react';
+import {  test, expect } from 'vitest'
+
+
+test("Header has a title", () => {
+    const {queryByText,} = render(<Header />);
+    expect(queryByText("Book Store")).toBeTruthy();
+});
