1dc5ff7174c98bb23112f26b79db7c819e869ca7
Antoine <antoine.gjeloshaj@fedasil.be>
2024-12-10 18:20:54 +0100

    Add style to book item

--- frontend/src/components/BookItem.jsx
+++ frontend/src/components/BookItem.jsx
@@ -1,11 +1,38 @@
+import styled from "styled-components";
 
+const StyledItem = styled.div`
+    width: 150px;
+    height: 200px;
+    padding-top: 5px;
+    background-color: #ffffb3;
+    &:hover {
+        background-color: #ffff87;
+        cursor: pointer;
+    }
+    text-align: center;
+`
+const StyledTitle = styled.p`
+   font-size: 20px;
+   font-style: bold;
+`
 
-export function BookItem({title, author, price}) {
+const StyledAuthor = styled.p`
+`
+
+const StyledPrice= styled.span`
+    border-radius: 100%;
+    padding: 5px;
+    background-color: #00ff00;
+    display: inline-block;
+    margin: 65% 80% 0;
+`
+
+export function BookItem({name, author, price}) {
     return (
-        <div>
-            <span>{title}</span>
-            <span>{author}</span>
-            <span>{price}</span>
-        </div>
+        <StyledItem>
+            <StyledTitle>{name}</StyledTitle>
+            <StyledAuthor>{author}</StyledAuthor>
+            <StyledPrice>{price}</StyledPrice>
+        </StyledItem>
     )
 }
