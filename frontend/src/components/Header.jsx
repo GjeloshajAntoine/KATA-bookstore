276323487fc0548f53285a531422de8bd39469ea
Antoine <antoine.gjeloshaj@fedasil.be>
2024-12-10 18:42:52 +0100

    Add link and style to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,24 @@
-import React from "react"
+import { Link } from 'react-router'
+import styled from 'styled-components';
+
+const StyledHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 0 5%;
+
+`;
+
+const StyledCart = styled.p`
+    float right;
+`;
 
 export function Header() {
-    return( <div>
-        <h1>Book Store</h1>
-    </div>);
+    return( <StyledHeader>
+        <Link to="/" >
+            <h1>Book Store</h1>
+        </Link>
+        <Link to="/cart">
+            <StyledCart>Cart</StyledCart>
+        </Link>
+    </StyledHeader>);
 }
