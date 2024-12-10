351c107e54e86f8488adec4f150b4e097e32f3ae
Antoine <antoine.gjeloshaj@fedasil.be>
2024-12-10 14:01:57 +0100

    Add book model

---
+++ Backend/src/main/java/com/example/BookStore/Models/Book.java
@@ -0,0 +1,38 @@
+package com.example.BookStore.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String author;
+
+    public Book() {
+
+    }
+
+    public Book(String name, String author) {
+        this.name = name;
+        this.author = author;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+}
